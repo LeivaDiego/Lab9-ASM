9f73e1bacf29afded48def24ac32c8698302a4a1
LeivaDiego <lei21752@uvg.edu.gt>
2023-05-23 17:46:32 -0600

    Variables v1
    Creación de variables: clientes, meses, NIT, montos e IVA.

--- Laboratorio9_PO_DL/Laboratorio9_PO_DL/Lab9.asm
+++ Laboratorio9_PO_DL/Laboratorio9_PO_DL/Lab9.asm
@@ -23,7 +23,42 @@ printf proto c : vararg
 scanf  proto c : vararg
 
 .data
+; Creación de Clientes
+clt1     BYTE    "Pablo",0
+clt2     BYTE    "Diego",0
+clt3     BYTE    "Juana",0
+clt4     BYTE    "Elisa",0
+clt5     BYTE    "Luisa",0
+clt6     BYTE    "Celia",0
+clt7     BYTE    "Rocio",0
+clt8     BYTE    "Lidia",0
+clt9     BYTE    "Maria",0
+clt10    BYTE    "Dylan",0
+clt11    BYTE    "Danna",0
+clt12    BYTE    "Elian",0
 
+; Creación de Meses
+mes1     BYTE    "Enero"        ,0
+mes2     BYTE    "Febrero"      ,0
+mes3     BYTE    "Marzo"        ,0
+mes4     BYTE    "Abril"        ,0
+mes5     BYTE    "Mayo"         ,0
+mes6     BYTE    "Junio"        ,0
+mes7     BYTE    "Julio"        ,0
+mes8     BYTE    "Agosto"       ,0
+mes9     BYTE    "Septiembre"   ,0
+mes10    BYTE    "Octubre"      ,0
+mes11    BYTE    "Noviembre"    ,0
+mes12    BYTE    "Diciembre"    ,0
+
+; Creación de NITs
+nit     DWORD   3126897, 9875796, 4597628, 3612548, 3612379, 9865731, 3645872, 6978546, 3210547, 2698756, 9867582, 3604879
+
+; Creación de Montos
+mon     DWORD   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+
+; Creación de IVA
+iva     DWORD   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
 
 .code
 
